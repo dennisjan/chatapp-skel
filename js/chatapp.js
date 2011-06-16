d9ca5a434e5fa19bd8d732406ac6aa7dae2ed832
Dennis-Jan Broerse <dennisjan@ibuildings.nl>
2011-06-16 16:19:57 +0200

    Implemented a lot

--- js/chatapp.js
+++ js/chatapp.js
@@ -7,10 +7,56 @@
  */
 window.ChatApp = { };
 
+jQuery.fn.sortElements = (function(){
+
+    var sort = [].sort;
+
+    return function(comparator, getSortable) {
+
+        getSortable = getSortable || function(){return this;};
+
+        var placements = this.map(function(){
+
+            var sortElement = getSortable.call(this),
+                parentNode = sortElement.parentNode,
+
+                // Since the element itself will change position, we have
+                // to have some way of storing its original position in
+                // the DOM. The easiest way is to have a 'flag' node:
+                nextSibling = parentNode.insertBefore(
+                    document.createTextNode(''),
+                    sortElement.nextSibling
+                );
+
+            return function() {
+
+                if (parentNode === this) {
+                    throw new Error(
+                        "You can't sort elements if any one is a descendant of another."
+                    );
+                }
+
+                // Insert before flag:
+                parentNode.insertBefore(this, nextSibling);
+                // Remove flag:
+                parentNode.removeChild(nextSibling);
+
+            };
+
+        });
+
+        return sort.call(this, comparator).each(function(i){
+            placements[i].call(getSortable.call(this));
+        });
+
+    };
+
+})();
+
 /**
  * The server to connect to
  */
-window.ChatApp.serverUri = 'http://localhost:8080/';
+window.ChatApp.serverUri = 'http://10.0.1.114:8080/';
 
 /**
  * Message Model
@@ -231,3 +277,207 @@ window.ChatApp.parseISO8601 = function(str) {
 };
 
 /** Your code goes here! **/
+/**
+ * MessageList view
+ * ================
+ *
+ * This view is responsible for updating the list of messages.
+ * You must pass a 'collection' option, which should be an instance of
+ * MessageCollection
+ */
+window.ChatApp.MessageListView = Backbone.View.extend({
+    initialize : function() {
+        var self = this;
+        this.collection.bind('add', function(message) {
+           self.addMessage(message);
+        });
+    },
+
+    addMessage : function (message) {
+        var messageEl = this.$('.template').clone();
+        messageEl.removeClass('template');
+        //username
+        messageEl.find('div').text(message.get('nickName'));
+        //tijd
+        messageEl.find('time').text(message.get('dateTime').toTimeString());
+        //message
+        messageEl.find('p').text(message.get('message'));
+        // gravatar
+        messageEl.css({
+           backgroundImage: "url('" + message.get('gravatar') + "?s=55&d=retro')"
+        });
+        
+        this.el.find('ul').append(messageEl);
+    }
+});
+
+
+/**
+ * MessageInput view
+ * ================
+ *
+ * This view is responsible for the 'input' area, which allows the user to
+ * send a message to the chatroom.
+ *
+ * You must pass a 'connection' option, which should be an instance of
+ * ChatApp.connection 
+ */
+window.ChatApp.MessageInputView = Backbone.View.extend({
+    events : {
+        "submit form" : "sendMessage"
+    },
+
+    sendMessage : function(evt) {
+
+        evt.preventDefault();
+        var message = this.$('input[name=message]').val();
+        this.options.connection.message(message);
+    }
+});
+    
+/**
+ * UserList view
+ * ================
+ *
+ * This view is responsible for keeping the list of online users up to
+ * date. 
+ * You must pass a 'collection' option, which should be an instance of
+ * UserCollection
+ */
+window.ChatApp.UserListView = Backbone.View.extend({
+    initialize : function() {
+        var self = this;
+        this.collection.bind('add', function(user) {
+           self.addUser(user);
+        });
+        this.collection.bind('remove', function(user) {
+           self.removeUser(user);
+        });
+    },
+
+    addUser : function(user) {
+        var userEL = this.$('.template').clone();
+        userEL.removeClass('template');
+        userEL.text(user.get('nickName'));
+        userEL.attr('class', 'nick-' + user.get('nickName'));
+        // gravatar
+        userEL.css({
+           backgroundImage: "url('" + user.get('gravatar') + "?s=25&d=retro')"
+        });
+        this.el.append(userEL);
+        this.$('li').sortElements(function(a, b){
+            return $(a).text() > $(b).text() ? 1 : -1;
+        });
+   },
+
+   removeUser : function(user) {
+       $('.nick-' + user.get('nickName')).remove();
+       this.$('li').sortElements(function(a, b){
+            return $(a).text().toLowerCase() > $(b).text().toLowerCase() ? 1 : -1;
+        });
+   }
+
+});
+
+/**
+ * The WelcomeView is responsible for handling the login screen
+ */
+window.ChatApp.WelcomeView = Backbone.View.extend({
+
+    events : {
+        "submit form" : "connect"
+    },
+
+    connect : function(evt) {
+
+        evt.preventDefault();
+        this.el.hide();
+        var nickName = this.$('input[name=nickName]').val();
+        var email = this.$('input[name=email]').val();
+
+        this.trigger('connect', {
+            nickName : nickName,
+            email : email
+        });
+
+    }
+
+});
+
+/**
+ * The Application View
+ * ====================
+ *
+ * The Application View is basically the main Application controller, and
+ * is responsible for setting up all the other objects.
+ */
+window.ChatApp.Application = Backbone.View.extend({
+
+    messageCollection : null,
+    userCollection : null,
+
+    messageListView : null,
+    messageInputView : null,
+    userListView : null,
+    welcomeView : null,
+
+    connection : null,
+
+    nickName : null,
+    email : null,
+
+    el: 'body',
+
+    initialize : function() {
+
+        var self = this;
+
+        this.messageCollection = new ChatApp.MessageCollection();
+        this.userCollection = new ChatApp.UserCollection();
+
+
+
+        this.welcomeView = new ChatApp.WelcomeView({
+            el : this.$('section.welcome')
+        });
+        this.welcomeView.bind('connect', function(userInfo) {
+            self.nickName = userInfo.nickName;
+            self.email = userInfo.email;
+            self.initializeConnection();
+        });
+
+    },
+
+    initializeConnection : function() {
+
+        this.connection = new ChatApp.Connection(this.userCollection, this.messageCollection, this.nickName, this.email, ChatApp.serverUri);
+
+        this.messageListView = new ChatApp.MessageListView({
+            collection: this.messageCollection,
+            el : this.$('section.messages')
+        });
+        this.messageInputView = new ChatApp.MessageInputView({
+            connection: this.connection,
+            el: this.$('section.inputArea')
+        }); 
+        this.userListView = new ChatApp.UserListView({
+            collection: this.userCollection,
+            el: this.$('section.userList')
+        });
+
+
+    }
+
+});
+
+
+
+/**
+ * Using jQuery's DOM.ready to fire up the application.
+ */
+$(document).ready(function() {
+
+    window.ChatApp.application = new ChatApp.Application;
+
+
+});
